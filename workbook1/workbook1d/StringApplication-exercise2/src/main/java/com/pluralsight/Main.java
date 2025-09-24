bf37f35d992666e918fa994bbd3d291b7a7adc2e
nKusaka <nikkusaka@gmail.com>
2025-09-24 11:57:01 -0700

    Completed Step 2 of exercise 2

--- workbook1/workbook1d/StringApplication-exercise2/src/main/java/com/pluralsight/Main.java
+++ workbook1/workbook1d/StringApplication-exercise2/src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,8 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //Step 1
         String favoriteColor = "red";
         int startYearUp = 2025;
         char middleInitial = 'S';
@@ -15,6 +17,17 @@ public class Main {
         System.out.println("My middle initial is " + middleInitial);
         System.out.println("Do I have pets? " + hasPets);
         System.out.println(message);
+
+        //Step 2
+        int daysInAWeek = 7;
+        double coffeePrice = 4.99;
+        char favoriteLetter = 'N';
+        boolean isRaining = true;
+
+        System.out.println("\nThere are " + daysInAWeek + " in a week.");
+        System.out.println("The price of coffee at Starbucks is $" + coffeePrice);
+        System.out.println("My favorite letter is " + favoriteLetter);
+        System.out.println("Is it raining? " + isRaining);
 
     }
 }
