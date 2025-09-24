ceaebcaf7d3471ddb60ba92b3b98466e5d45bf2d
nKusaka <nikkusaka@gmail.com>
2025-09-24 11:54:37 -0700

    Completed Step 1 of exercise 2

---
+++ workbook1/workbook1d/StringApplication-exercise2/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String favoriteColor = "red";
+        int startYearUp = 2025;
+        char middleInitial = 'S';
+        boolean hasPets = true;
+        String message = "Have a nice day!";
+
+        System.out.println("My favorite color is " + favoriteColor);
+        System.out.println("I started YUU in " + startYearUp);
+        System.out.println("My middle initial is " + middleInitial);
+        System.out.println("Do I have pets? " + hasPets);
+        System.out.println(message);
+
+    }
+}
