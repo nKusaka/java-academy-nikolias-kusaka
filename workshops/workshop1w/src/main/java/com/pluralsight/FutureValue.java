477b504bfe8ac97412f163b7570f43bd4dc02329
nKusaka <nikkusaka@gmail.com>
2025-09-26 15:56:49 -0700

    Created futureValue method that calculates the expected growth of money after a certain amount of time.

--- workshops/workshop1w/src/main/java/com/pluralsight/FutureValue.java
+++ workshops/workshop1w/src/main/java/com/pluralsight/FutureValue.java
@@ -13,11 +13,17 @@ public class FutureValue {
         System.out.printf("How much is your initial deposit?: $");
         initialDeposit = read.nextInt();
 
-        System.out.printf("How much is your interest rate?: $");
+        System.out.printf("How much is your interest rate in decimal form?: $");
         interestRate = read.nextDouble();
 
         System.out.printf("How many years will you let your money grow?: $");
         years = read.nextInt();
 
+    }
+
+    public static double futureValue(int principal, double interestRate, int years) {
+        double futureValue = principal * Math.pow((1 + (interestRate / 365) ), (365 * years));
+
+        return futureValue;
     }
 }
