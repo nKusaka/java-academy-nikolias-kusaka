10dbef550611ff0796f26eec095057bf9b1d6e7f
nKusaka <nikkusaka@gmail.com>
2025-09-26 15:46:10 -0700

    Fixed error with calculation and added in display

--- workshops/workshop1w/src/main/java/com/pluralsight/Mortgage.java
+++ workshops/workshop1w/src/main/java/com/pluralsight/Mortgage.java
@@ -19,11 +19,17 @@ public class Mortgage {
 
         System.out.printf("Please enter the length of time to pay back the loan in years: ");
         loanLength = read.nextInt();
+
+        double mortgage = monthlyPayment(principal, interestRate, loanLength);
+        double totalInterest = totalInterest(mortgage, loanLength, principal);
+
+        display(mortgage, totalInterest);
     }
 
     public static double monthlyPayment(int principal, double interestRate, int loanLength) {
         int monthlyLoanLength = loanLength * 12;
-        double mortgage = principal * (interestRate * Math.pow((1 + interestRate), monthlyLoanLength) / (Math.pow((1 + interestRate), monthlyLoanLength) - 1));
+        double monthlyInterestRate = interestRate / 12;
+        double mortgage = principal * (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), monthlyLoanLength) / (Math.pow((1 + monthlyInterestRate), monthlyLoanLength) - 1));
 
         return mortgage;
     }
@@ -33,5 +39,12 @@ public class Mortgage {
         double totalInterest = (mortgage * monthlyLoanLength) - principal;
 
         return totalInterest;
+    }
+
+    public static void display(double mortgage, double totalInterest) {
+        System.out.printf("Your mortgage will be $%.2f" , mortgage);
+        System.out.println();
+        System.out.printf("Your total interest that will be paid will be $%.2f" , totalInterest);
+
     }
 }
