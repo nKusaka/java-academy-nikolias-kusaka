800c8e9e481faa0423c240eecb93061fd709458a
nKusaka <nikkusaka@gmail.com>
2025-09-26 10:35:09 -0700

    Practiced creating methods that output different messages and are called from the main method.

---
+++ workbook1/workbook1d/method-practice/src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+    }
+
+    // Method practice to output Hello, World! no parameters
+    public static void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    // Method practice to output Goodbye! no parameters
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+
+    // Method practice to output Good Morning! no parameters
+    public static void sayGoodMorning() {
+        System.out.println("Good morning!");
+    }
+}
