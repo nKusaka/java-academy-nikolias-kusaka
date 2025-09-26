d73f835232f7d2a71b31737bbd907fed244ef1c2
nKusaka <nikkusaka@gmail.com>
2025-09-26 15:32:58 -0700

    Added in total interest part of calculator

--- workshops/workshop1w/src/main/java/com/pluralsight/Mortgage.java
+++ workshops/workshop1w/src/main/java/com/pluralsight/Mortgage.java
@@ -17,15 +17,21 @@ public class Mortgage {
         System.out.printf("Please enter the interset rate in decimal form: ");
         interestRate = read.nextDouble();
 
-        System.out.printf("Please enter the length of time to pay back the loan: ");
+        System.out.printf("Please enter the length of time to pay back the loan in years: ");
         loanLength = read.nextInt();
     }
 
     public static double monthlyPayment(int principal, double interestRate, int loanLength) {
-        int monthlyLoanLength = loanLength * 12
+        int monthlyLoanLength = loanLength * 12;
         double mortgage = principal * (interestRate * Math.pow((1 + interestRate), monthlyLoanLength) / (Math.pow((1 + interestRate), monthlyLoanLength) - 1));
 
         return mortgage;
     }
 
+    public static double totalInterest(double mortgage, int loanLength, int principal) {
+        int monthlyLoanLength = loanLength * 12;
+        double totalInterest = (mortgage * monthlyLoanLength) - principal;
+
+        return totalInterest;
+    }
 }
