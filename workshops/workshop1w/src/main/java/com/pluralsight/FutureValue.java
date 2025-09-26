c81ef2453aaace1b331f676784a3d9d993f64c58
nKusaka <nikkusaka@gmail.com>
2025-09-26 16:03:03 -0700

    Cleaned up some of the messages as well as added a new method to display future value and interest earned. Also called each method to show that they work

--- workshops/workshop1w/src/main/java/com/pluralsight/FutureValue.java
+++ workshops/workshop1w/src/main/java/com/pluralsight/FutureValue.java
@@ -13,12 +13,16 @@ public class FutureValue {
         System.out.printf("How much is your initial deposit?: $");
         initialDeposit = read.nextInt();
 
-        System.out.printf("How much is your interest rate in decimal form?: $");
+        System.out.printf("How much is your interest rate in decimal form?: ");
         interestRate = read.nextDouble();
 
         System.out.printf("How many years will you let your money grow?: $");
         years = read.nextInt();
 
+        double futureValue = futureValue(initialDeposit, interestRate, years);
+        double interestEarned = interestEarned(initialDeposit, futureValue);
+
+        display(futureValue, interestEarned);
     }
 
     public static double futureValue(int principal, double interestRate, int years) {
@@ -33,5 +37,11 @@ public class FutureValue {
         return interestEarned;
     }
 
+    public static void display(double futureValue, double interestEarned) {
+        System.out.printf("Your expected future value is: $%.2f" , futureValue);
+        System.out.println();
+        System.out.printf("Your expected interest earned value is: $%.2f" , interestEarned);
+
+    }
 
 }
