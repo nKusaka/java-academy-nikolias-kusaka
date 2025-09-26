f146274814a38024d5315f425521fdf2f3bb99af
nKusaka <nikkusaka@gmail.com>
2025-09-26 15:24:03 -0700

    Added in, prompt user for input.

--- workshops/workshop1w/src/main/java/com/pluralsight/Mortgage.java
+++ workshops/workshop1w/src/main/java/com/pluralsight/Mortgage.java
@@ -1,8 +1,24 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Mortgage {
     public static void main(String[] args) {
 
+        Scanner read = new Scanner(System.in);
 
+        int principal;
+        double interestRate;
+        int loanLength;
+        double monthlyPayment;
+
+        System.out.printf("Please enter the principal amount of the loan: $");
+        principal = read.nextInt();
+
+        System.out.printf("Please enter the interset rate in decimal form: ");
+        interestRate = read.nextDouble();
+
+        System.out.printf("Please enter the length of time to pay back the loan: ");
+        loanLength = read.nextInt();
     }
+
 }
