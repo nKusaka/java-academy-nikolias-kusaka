0b6c0ba737faeaee367e049bcba366e94dce0b1d
nKusaka <nikkusaka@gmail.com>
2025-09-29 09:36:11 -0700

    Created a grade checker where the user inputs an integer value for their grade and then turns that integer value into a letter grade and outputs it with a message for the user.

---
+++ demos-teacher/demo-week2/src/main/java/com/pluralsight/GradeChecker.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class GradeChecker {
+    public static void main(String[] args) {
+
+        // Created scanner variable and also other variables for use in the program
+        Scanner read = new Scanner(System.in);
+
+        // Gets user input and stores in int variable "examScore"
+        System.out.printf("What scored did you get on your exam?: ");
+        int examScore = read.nextInt();
+
+        // Different outputs for different scores
+        if (examScore >= 90) {
+            System.out.printf("You got an A! Nice job!");
+        }
+        else if (examScore >= 80) {
+            System.out.printf("You got a B! Pretty good!");
+        }
+        else if (examScore >= 70) {
+            System.out.printf("You got a C! Not bad, could use some work.");
+        }
+        else if (examScore >= 60) {
+            System.out.printf("You got a D, keep working on it you can do better.");
+        }
+        else {
+            System.out.printf("You got an F, unlucky.");
+        }
+    }
+}
