7c5764031be58e71921134ac12c6f5d50d30d83f
nKusaka <nikkusaka@gmail.com>
2025-09-29 14:37:04 -0700

    practiced doing string methods.

---
+++ demos-teacher/workbook2-strings-and-arrays/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String movieTitle = "spiderman across the spiderverse";
+
+        System.out.println(movieTitle.toUpperCase());
+    }
+}
