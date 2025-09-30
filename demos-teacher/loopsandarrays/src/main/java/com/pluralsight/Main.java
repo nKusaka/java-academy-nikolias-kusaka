3b4daf50773c391ea4323d1858e4c733359bbe81
nKusaka <nikkusaka@gmail.com>
2025-09-30 08:38:00 -0700

    Practice with for loops

---
+++ demos-teacher/loopsandarrays/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i ++) {
+            System.out.println((i + 1) + ". I will be a good developer ");
+        }
+    }
+}
