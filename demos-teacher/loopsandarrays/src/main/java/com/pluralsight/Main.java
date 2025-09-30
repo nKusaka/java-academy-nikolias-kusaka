2090758820338ce6dd264c6308eb8914915454a8
nKusaka <nikkusaka@gmail.com>
2025-09-30 09:13:28 -0700

    practiced with while loops/ do while loops and also using new method thread.sleep in try and catch blocks.

--- demos-teacher/loopsandarrays/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/loopsandarrays/src/main/java/com/pluralsight/Main.java
@@ -1,13 +1,36 @@
 package com.pluralsight;
+import java.lang.*;
+import java.util.Scanner;
 
 //for loop practice
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
         for(int i = 1; i <= 5; i++){
             if(i % 2 == 0) {
                 System.out.println(i);
             }
         }
+
+        int j = 10;
+        while(j >= 1) {
+            System.out.println("You have " + j + " seconds left");
+            try {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            j--;
+
+        }
+        String exitLoop;
+        do {
+            System.out.printf("Type exit to exit loop: ");
+            exitLoop = read.nextLine();
+        }
+        while(!exitLoop.contains("exit"));
 
     }
 }
