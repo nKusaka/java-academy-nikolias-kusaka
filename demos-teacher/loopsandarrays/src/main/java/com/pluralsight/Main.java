532cf778713f71f96d5e057ac60c519c0bbddff5
nKusaka <nikkusaka@gmail.com>
2025-09-30 10:45:52 -0700

    created a method to check if email addresses are valid or not using a boolean method.

--- demos-teacher/loopsandarrays/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/loopsandarrays/src/main/java/com/pluralsight/Main.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 //for loop practice
 public class Main {
     public static void main(String[] args) {
-        String myEmailAddress = "rmailjard@yEARup.OrG";
-
-        int validation = myEmailAddress.indexOf("@");
-        String check = myEmailAddress.substring(validation+1);
-        if(myEmailAddress.substring(validation + 1).equalsIgnoreCase("yearup.org")) {
+        String myEmailAddress = "rmailjard@yEARup.OrGg";
+        if(validateEmailAddress(myEmailAddress)) {
             System.out.println("Correct e-mail address");
         } else {
             System.out.println("Wrong e-mail address");
         }
+
     }
+
+    public static boolean validateEmailAddress(String email) {
+        int validation = email.indexOf("@");
+        String emailSymbolValidation = email.substring(validation + 1);
+        boolean check;
+
+        if(email.substring(validation + 1).equalsIgnoreCase("yearup.org") || email.substring(validation + 1).equalsIgnoreCase(("skills4-it.nl"))) {
+            check = true;
+        } else {
+            check = false;
+        }
+        return check;
+    }
+
 }
