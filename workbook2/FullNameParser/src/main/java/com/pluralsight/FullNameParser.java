5168080c24f318711baf73e07529fc9074229331
nKusaka <nikkusaka@gmail.com>
2025-09-30 13:50:22 -0700

    name parser now works properly by taking apart a persons full name with or without middle name.

--- workbook2/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ workbook2/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -7,12 +7,14 @@ public class FullNameParser {
 
         System.out.printf("Please enter your name first and last (optionally with middle initial): ");
         String fullName = read.nextLine();
-        fullName.trim();
+        fullName = fullName.trim();
 
 
         System.out.println("First name: " + fullName.substring(0, fullName.indexOf(' ')));
 
-       System.out.println("Last name: " + lastName);
+        System.out.println("Middle name: " + fullName.substring(fullName.indexOf(' '), fullName.lastIndexOf(' ')));
+
+        System.out.println("Last Name: " + fullName.substring(fullName.lastIndexOf(' ')));
     }
 }
 
