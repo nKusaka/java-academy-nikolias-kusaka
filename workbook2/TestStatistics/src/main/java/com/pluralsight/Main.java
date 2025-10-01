d4d2c0698233b14179eebaa95ccc689ffe962667
nKusaka <nikkusaka@gmail.com>
2025-10-01 15:45:57 -0700

    Retrieved the lowest and highest scores from the array.

--- workbook2/TestStatistics/src/main/java/com/pluralsight/Main.java
+++ workbook2/TestStatistics/src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,5 @@
 package com.pluralsight;
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         int[] TestScore = {91, 92, 93, 94, 89, 100, 72, 69, 83, 99};
 
+        Arrays.sort(TestScore);
 
+        System.out.println("The lowest score is: " + TestScore[0]);
+        System.out.println("The highest score is: " + TestScore[9]);
     }
 }
