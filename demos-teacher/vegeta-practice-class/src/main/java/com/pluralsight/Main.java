808eb349f2009e7998bcafdd4247fe97dc94282a
nKusaka <nikkusaka@gmail.com>
2025-10-01 15:44:01 -0700

    created an array of 10 test scores

--- demos-teacher/vegeta-practice-class/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/vegeta-practice-class/src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,24 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
+        SuperSayin ss1 = new SuperSayin();
+        ss1.name = "Goku";
+        ss1.powerlevel = 1000;
+        ss1.PowerUp();
+
+        SuperSayin ss2 = new SuperSayin();
+        ss2.name = "Vegeta";
+        ss2.powerlevel = 998;
+        ss2.PowerUp();
+        ss2.Attack(ss1);
+        ss1.Stats();
+        ss2.Attack(ss1);
+        ss1.Stats();
+
+        ss1.Heal();
+        ss1.Stats();
+
+        ss1.superSaiyanTwo();
 
     }
 }
---
+++ workbook2/TestStatistics/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] TestScore = {91, 92, 93, 94, 89, 100, 72, 69, 83, 99};
+
+
+    }
+}
