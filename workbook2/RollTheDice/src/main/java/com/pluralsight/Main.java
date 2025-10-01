0c854278323688b69081904fe91e2929e6382405
nKusaka <nikkusaka@gmail.com>
2025-10-01 13:26:06 -0700

    everything works just need to display the counters now

--- workbook2/RollTheDice/src/main/java/com/pluralsight/Main.java
+++ workbook2/RollTheDice/src/main/java/com/pluralsight/Main.java
@@ -4,7 +4,56 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        RollTheDice dice1 = new RollTheDice();
-        RollTheDice dice2 = new RollTheDice();
+
+        RollTheDice diceOne = new RollTheDice();
+        RollTheDice diceTwo = new RollTheDice();
+        int twoCounter = 0;
+        int fourCounter = 0;
+        int sixCounter = 0;
+        int sevenCounter = 0;
+
+        for (int i = 0; i < 100; i++) {
+            diceOne.roll();
+            diceTwo.roll();
+
+            System.out.println("Roll " + (i + 1) + ": " + diceOne.getNumber() + " - " + diceTwo.getNumber() + " Sum: " + (diceOne.getNumber() + diceTwo.getNumber()));
+            // Checking for how many times two pops up as a role
+            if ((diceOne.getNumber() + diceTwo.getNumber() == 2)) {
+                twoCounter += 1;
+
+            } else if (diceOne.getNumber() == 2 && diceTwo.getNumber() == 2) {
+                twoCounter += 2;
+
+            } else if (diceOne.getNumber() == 2 || diceTwo.getNumber() == 2) {
+                twoCounter += 1;
+            }
+
+            // Checking for how many times four pops up as a role
+            if ((diceOne.getNumber() + diceTwo.getNumber() == 4)) {
+                fourCounter += 1;
+
+            } else if (diceOne.getNumber() == 4 && diceTwo.getNumber() == 4) {
+                fourCounter += 2;
+
+            } else if (diceOne.getNumber() == 4 || diceTwo.getNumber() == 4) {
+                fourCounter += 1;
+            }
+
+            // Checking for how many times six pops up as a role
+            if ((diceOne.getNumber() + diceTwo.getNumber() == 6)) {
+                sixCounter += 1;
+
+            } else if (diceOne.getNumber() == 6 && diceTwo.getNumber() == 6) {
+                sixCounter += 2;
+
+            } else if (diceOne.getNumber() == 6 || diceTwo.getNumber() == 6) {
+                sixCounter += 1;
+            }
+
+            // Checking for how many times seven pops up as a role
+            if ((diceOne.getNumber() + diceTwo.getNumber() == 7)) {
+                sevenCounter += 1;
+            }
+        }
     }
 }
