ba99a5fc8dc9edecec0e987ebbf5957d86b8dfa4
Nik <nikkusaka@gmail.com>
2025-10-02 08:50:55 -0700

    -Created full array of six pre loaded car objects.

--- workbook2/VehicleInventory/src/main/java/com/pluralsight/Main.java
+++ workbook2/VehicleInventory/src/main/java/com/pluralsight/Main.java
@@ -4,7 +4,12 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Vehicle[] vehicles = {101121,"Ford Explorer","Red",45000,13500, };
+        Vehicle[] vehicles = {new Vehicle(101121,"Ford Explorer","Red",45000,13500 )
+        , new Vehicle(101122,"Toyota Camry","Blue",60000,11000)
+        , new Vehicle(101123,"Chevrolet Malibu","Black",50000,9700 )
+        , new Vehicle(101124,"Honda Civic","White",70000,7500 )
+        , new Vehicle(101125,"Subaru Outback","Green",55000,14500 )
+        , new Vehicle(101126,"Jeep Wrangler","Yellow",30000,16000)};
 
     }
 }
