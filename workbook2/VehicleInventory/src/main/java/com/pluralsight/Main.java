36a54b2e19990b29a8393b935966ced9a5ee427d
Nik <nikkusaka@gmail.com>
2025-10-02 09:01:37 -0700

    -Remade array to hold 20 cars -Preloaded array to have 6 cars in it

--- workbook2/VehicleInventory/src/main/java/com/pluralsight/Main.java
+++ workbook2/VehicleInventory/src/main/java/com/pluralsight/Main.java
@@ -4,12 +4,15 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Vehicle[] vehicles = {new Vehicle(101121,"Ford Explorer","Red",45000,13500 )
-        , new Vehicle(101122,"Toyota Camry","Blue",60000,11000)
-        , new Vehicle(101123,"Chevrolet Malibu","Black",50000,9700 )
-        , new Vehicle(101124,"Honda Civic","White",70000,7500 )
-        , new Vehicle(101125,"Subaru Outback","Green",55000,14500 )
-        , new Vehicle(101126,"Jeep Wrangler","Yellow",30000,16000)};
+
+        Vehicle[] vehicles= new Vehicle[20];
+
+        vehicles[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        vehicles[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        vehicles[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        vehicles[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        vehicles[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        vehicles[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
 
     }
 }
