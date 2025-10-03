12d5f073fb2f6a9fe1396a9c3b227688d32aa592
Nik <nikkusaka@gmail.com>
2025-10-03 13:42:50 -0700

    -Created setters in book class -Created getters in book class

--- workshops/neighborhood-library/src/main/java/com/pluralsight/Book.java
+++ workshops/neighborhood-library/src/main/java/com/pluralsight/Book.java
@@ -27,4 +27,48 @@ public class Book {
         checkedOutTo = "";
     }
 
+    // Setters
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setISBN(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsCheckedOut(boolean isCheckedOut) {
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public void setCheckOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    // Getters
+
+    public int getID() {
+        return this.id;
+    }
+
+    public String getISBN() {
+        return this.isbn;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public boolean getIsCheckedOut() {
+        return this.isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return this.checkedOutTo;
+    }
+
+
 }
