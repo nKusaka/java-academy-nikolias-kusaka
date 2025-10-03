fd977122710ceb8aa52957a6ebde4f4a5225ff43
Nik <nikkusaka@gmail.com>
2025-10-03 13:34:52 -0700

    -Started new library project

--- demos-teacher/TodoApp/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/TodoApp/src/main/java/com/pluralsight/Main.java
@@ -14,5 +14,24 @@ public class Main {
         // Set new task as completed and print new message
         t.markCompleted();
         System.out.println(t);
+
+        // Create an array of 3 tasks
+        Task[] tasks = new Task[3];
+
+        // Initialize 3 tasks in tasks array
+        tasks[0] = new Task("Learn the basics of Classes and Methods in Java");
+        tasks[1] = new Task("Fill out this Year Up online form. It's feedback for Yearup about how you guys think the lessons are doing");
+        tasks[2] = new Task("Share your most fun exercise we did in class");
+
+        // Step 3: mark task('s) as completed
+        tasks[1].markCompleted();
+        tasks[2].markCompleted();
+        tasks[0].markCompleted();
+        // Step 4: loop through the array and print each task
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
     }
+
+
 }
