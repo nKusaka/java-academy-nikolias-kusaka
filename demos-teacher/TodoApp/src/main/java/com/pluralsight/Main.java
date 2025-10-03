8cb146db6794649e6486407458f6dd39ce14e1b6
Nik <nikkusaka@gmail.com>
2025-10-03 11:30:44 -0700

    -Thought up some ideas for variables -Implemented variables in Task class -Created method in Task class to mark a task as completed -Overrided String method in Task class to display tasks as completed when called -Tested to make sure everything works in main.java

--- demos-teacher/TodoApp/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/TodoApp/src/main/java/com/pluralsight/Main.java
@@ -4,5 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our Todo App");
+
+        // Create new object from task class
+        Task t = new Task("Learn Java");
+
+        // Print out if the task is completed or not
+        System.out.println(t);
+
+        // Set new task as completed and print new message
+        t.markCompleted();
+        System.out.println(t);
     }
 }
