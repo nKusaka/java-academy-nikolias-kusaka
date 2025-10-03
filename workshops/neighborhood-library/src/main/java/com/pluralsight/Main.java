f36a9b7807fe9aa49807c768b217d195e9134f01
Nik <nikkusaka@gmail.com>
2025-10-03 14:44:55 -0700

    -updated isbns for books

--- workshops/neighborhood-library/src/main/java/com/pluralsight/Main.java
+++ workshops/neighborhood-library/src/main/java/com/pluralsight/Main.java
@@ -30,26 +30,26 @@ public class Main {
 
     public static Book[] initializeLibrary() {
         Book[] books = new Book[20];
-        books[0] = new Book(1, "1", "Harry Potter");
-        books[1] = new Book(2, "2", "Percy Jackson");
-        books[2] = new Book(3, "3", "Bible");
-        books[3] = new Book(4,"4","Demon Slayer");
-        books[4] = new Book(5,"5","Dragonball");
-        books[5] = new Book(6, "6", "Dragonball Z");
-        books[6] = new Book(7, "7", "Bleach");
-        books[7] = new Book(8, "8", "Java for Beginners");
-        books[8] = new Book(9, "9", "C++ for Beginners");
-        books[9] = new Book(10, "10", "Round Table Pizza");
-        books[10] = new Book(11, "11", "In N Out Burger");
-        books[11] = new Book(12, "12", "Amazon Warehouse");
-        books[12] = new Book(13, "13", "Twitch");
-        books[13] = new Book(14, "14", "League of Legends");
-        books[14] = new Book(15, "15", "Valorant");
-        books[15] = new Book(16, "16", "Megabonk");
-        books[16] = new Book(17, "17", "Steam");
-        books[17] = new Book(18, "18", "IntelliJ IDEA");
-        books[18] = new Book(19, "19", "Python for Beginners");
-        books[19] = new Book(20, "20", "Sleep for Dummy's");
+        books[0] = new Book(1, "9783061069094", "Harry Potter");
+        books[1] = new Book(2, "9795178347446", "Percy Jackson");
+        books[2] = new Book(3, "9795872059423", "Bible");
+        books[3] = new Book(4,"9795238310953","Demon Slayer");
+        books[4] = new Book(5,"9780115958212","Dragonball");
+        books[5] = new Book(6, "9781752518869", "Dragonball Z");
+        books[6] = new Book(7, "9793724252688", "Bleach");
+        books[7] = new Book(8, "9795225941849", "Java for Beginners");
+        books[8] = new Book(9, "9784766958959", "C++ for Beginners");
+        books[9] = new Book(10, "9792912005228", "Round Table Pizza");
+        books[10] = new Book(11, "9794507254592", "In N Out Burger");
+        books[11] = new Book(12, "9794234107291", "Amazon Warehouse");
+        books[12] = new Book(13, "9791067180002", "Twitch");
+        books[13] = new Book(14, "9793818070143", "League of Legends");
+        books[14] = new Book(15, "9792974405578", "Valorant");
+        books[15] = new Book(16, "9784298976124", "Megabonk");
+        books[16] = new Book(17, "9794914822674", "Steam");
+        books[17] = new Book(18, "9792764605256", "IntelliJ IDEA");
+        books[18] = new Book(19, "9794357809638", "Python for Beginners");
+        books[19] = new Book(20, "9785681230601", "Sleep for Dummy's");
 
         return books;
     }
@@ -97,7 +97,7 @@ public class Main {
                           +"========================================");
 
         for(int i = 0; i < 20; i++) {
-            sout
+            System.out.println((i + 1) + ". " + library[i]);
         }
     }
 
