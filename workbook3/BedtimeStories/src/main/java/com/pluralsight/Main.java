7a95074e96cdeaa268e6cc4d20adb2d9de9caa29
Nik <nikkusaka@gmail.com>
2025-10-06 14:28:38 -0700

    -Fixed counter error (counter was only displaying a 1, forgot to increment after each loop through)

--- workbook3/BedtimeStories/src/main/java/com/pluralsight/Main.java
+++ workbook3/BedtimeStories/src/main/java/com/pluralsight/Main.java
@@ -1,22 +1,39 @@
 package com.pluralsight;
 import java.util.Scanner;
-import java.io.BufferedReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Create new scanner and buffered reader
+        // Create new scanner and counter for while loop
         Scanner read = new Scanner(System.in);
-        BufferedReader bufferReader = new BufferedReader(read);
+        int counter = 1;
 
         // Prompt user for file name
-        System.out.println("What story would you like to see?");
-        System.out.println("Please use the correct format");
-        System.out.printf("Please enter one of the follow: goldilocks.txt," +
-                "hansel_and_gretel.txt, or mary_had_a_little_lamb.txt");
+        System.out.printf("Please enter one of the following, goldilocks.txt," +
+                "\nhansel_and_gretel.txt, or mary_had_a_little_lamb.txt: ");
 
         // Record user input
         String bedtimeStory = read.nextLine();
+
+        // have file reader read users input file and print it out
+        try {
+
+            // Create new filereader, bufferreader, and string input to read the file
+            FileReader fileReader = new FileReader(bedtimeStory);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String input;
+
+            // while loop to read the file completely
+            while((input = bufferedReader.readLine()) != null) {
+
+                System.out.println(counter + ". " + input);
+                counter++;
+            }
+        }
+        catch(IOException e) {
+            System.out.println("File unreachable");
+        }
 
     }
 }
