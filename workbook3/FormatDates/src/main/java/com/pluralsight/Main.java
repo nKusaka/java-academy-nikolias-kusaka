b4aed27b45ec549f0bb74cb5dd05c191154c85fc
Nik <nikkusaka@gmail.com>
2025-10-07 09:23:23 -0700

    -Started new project formatting dates and times

---
+++ workbook3/FormatDates/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatterBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Create date time formatter and localdatetime object
+        LocalDate
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+
+
+
+    }
+}
