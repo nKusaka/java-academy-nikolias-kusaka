e365e92f8473b522d1feae675680819c04ea8605
Nik <nikkusaka@gmail.com>
2025-10-07 09:25:36 -0700

    -Outputs current date in form MM/dd/yyyy

--- workbook3/FormatDates/src/main/java/com/pluralsight/Main.java
+++ workbook3/FormatDates/src/main/java/com/pluralsight/Main.java
@@ -8,11 +8,14 @@ import java.time.format.DateTimeFormatterBuilder;
 public class Main {
     public static void main(String[] args) {
 
-        // Create date time formatter and localdatetime object
-        LocalDate
+        // Create date time formatter and localdatetime object as well as
+        // string to hold date and time
+        LocalDate dateToday = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDate = dateToday.format(formatter);
 
-
+        // Output date and time
+        System.out.println(formattedDate);
 
 
     }
