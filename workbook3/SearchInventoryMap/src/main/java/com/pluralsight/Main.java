f71cacda400275b3d6d95fe52ec59ff078f2d459
Nik <nikkusaka@gmail.com>
2025-10-08 13:15:11 -0700

    -Changed float to double in class added csv file to main program folder

--- workbook3/SearchInventoryMap/src/main/java/com/pluralsight/Main.java
+++ workbook3/SearchInventoryMap/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,13 @@
 package com.pluralsight;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main {ing[] args) {
 
+    public static void main(Str
+    }
+
+    public static Product loadInventory(Product inventory) {
+        FileReader
     }
 }
