7d09d26ca20432af34c10bcb79e1a16d127d78d3
Nik <nikkusaka@gmail.com>
2025-10-08 13:15:11 -0700

    -Changed float to double in class added csv file to main program folder

--- workbook3/SearchInventoryMap/src/main/java/com/pluralsight/Product.java
+++ workbook3/SearchInventoryMap/src/main/java/com/pluralsight/Product.java
@@ -4,9 +4,9 @@ public class Product {
 
     private int id;
     private String name;
-    private float price;
+    private double price;
 
-    public Product(int id, String name, float price) {
+    public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -17,7 +17,7 @@ public class Product {
     public String getName() {
         return this.name;
     }
-    public float getPrice() {
+    public double getPrice() {
         return this.price;
     }
 }
