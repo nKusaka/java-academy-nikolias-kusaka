373c5a9f3569810b80bbe919ac0fd092aebf4525
Nik <nikkusaka@gmail.com>
2025-10-20 13:36:10 -0700

    -Created a Room class for the hotel

---
+++ workbook4/HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+public class Room {
+
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+    private boolean isAvailable;
+
+    public Room(int numberOfBeds, double price, boolean isDirty, boolean isOccupied, boolean isAvailable) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isDirty = isDirty;
+        this.isOccupied = isOccupied;
+        this.isAvailable = isAvailable;
+    }
+
+    public Room() {
+
+    }
+
+    public int getNumberOfBeds() {
+        return this.numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return this.isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.isDirty = dirty;
+    }
+
+    public boolean isAvailable() {
+        return this.isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+}
