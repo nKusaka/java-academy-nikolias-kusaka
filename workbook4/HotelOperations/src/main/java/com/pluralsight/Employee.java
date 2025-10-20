2afbad03f63c8e50a75b496f0c021f3455f2dd50
Nik <nikkusaka@gmail.com>
2025-10-20 13:46:50 -0700

    -Created an employee class for the hotel

---
+++ workbook4/HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,82 @@
+package com.pluralsight;
+
+public class Employee {
+
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public Employee() {
+
+    }
+
+    public int getEmployeeId() {
+        return this.employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return this.payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getTotalPay() {
+        return this.hoursWorked * this.payRate;
+    }
+
+    public double getRegularHours() {
+        if (this.hoursWorked > 40) {
+            return 40;
+        } else {
+            return this.hoursWorked;
+        }
+    }
+
+    public double getOvertimeHours() {
+        if (this.hoursWorked > 40) {
+            return this.hoursWorked - 40;
+        } else {
+            return 0;
+        }
+    }
+}
