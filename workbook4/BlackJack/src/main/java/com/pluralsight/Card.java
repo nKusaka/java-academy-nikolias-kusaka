a1e1c355fb4895e6597fc5932e0015c491a30550
Nik <nikkusaka@gmail.com>
2025-10-21 14:56:21 -0700

    -Created card class and copied deck class from workbook so now the deck can be made up of cards

--- workbook4/BlackJack/src/main/java/com/pluralsight/Card.java
+++ workbook4/BlackJack/src/main/java/com/pluralsight/Card.java
@@ -2,4 +2,47 @@ package com.pluralsight;
 
 public class Card {
 
+    private String suit;
+    private String value;
+    private boolean isFaceUp;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.isFaceUp = false;
+    }
+
+    public String getSuit() {
+        if(isFaceUp) {
+            return this.suit;
+        }
+        return "#";
+    }
+
+    public String getValue() {
+        if(isFaceUp) {
+            return this.value;
+        }
+        return "#";
+    }
+
+    public int getPointValue() {
+        if(isFaceUp) {
+            switch(getValue()){
+                case "A":
+                    return 11;
+                case "K", "Q", "J":
+                    return 10;
+            }
+        }
+        return Integer.parseInt(getValue());
+    }
+
+    public boolean isFaceUp() {
+        return this.isFaceUp;
+    }
+
+    public void flip() {
+        isFaceUp = !isFaceUp;
+    }
 }
