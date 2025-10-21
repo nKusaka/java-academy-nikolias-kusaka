db727965932a40914df6907737209cb16372d870
Nik <nikkusaka@gmail.com>
2025-10-21 15:06:21 -0700

    -added a hand class for the player and a dealer class for them to play against

--- workbook4/BlackJack/src/main/java/com/pluralsight/Dealer.java
+++ workbook4/BlackJack/src/main/java/com/pluralsight/Dealer.java
@@ -1,5 +1,40 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealer {
 
+    private ArrayList<Card> cards;
+
+    public Dealer() {
+        cards = new ArrayList<>();
+    }
+
+    public void Deal(Card card) {
+        cards.add(card);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public int getValue() {
+        int value = 0;
+        boolean isAce = false;
+
+        for (Card cardInHand: cards) {
+            cardInHand.flip();
+
+            if (cardInHand.getPointValue() == 11) {
+                isAce = true;
+            }
+
+            value += cardInHand.getPointValue();
+        }
+
+        if (isAce && value > 21) {
+            value -= 10;
+        }
+        return value;
+    }
 }
