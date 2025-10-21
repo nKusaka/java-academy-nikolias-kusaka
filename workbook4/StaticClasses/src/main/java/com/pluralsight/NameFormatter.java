013a0243bff6e641a591a1e930263ee589f95f71
Nik <nikkusaka@gmail.com>
2025-10-21 14:33:46 -0700

    -format methods now works correctly and are correctly formatted

--- workbook4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
+++ workbook4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -4,51 +4,24 @@ import java.util.ArrayList;
 public class NameFormatter {
 
     public static String format(String firstName, String lastName) {
-        return lastName + " " + firstName;
+        return lastName + ", " + firstName;
     }
 
     public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
-        return lastName + " " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
     }
 
     public static String format(String fullName) {
-            String prefix = "";
-            String firstName = "";
-            String middleName = "";
-            String lastName = "";
-            String suffix = "";
-            ArrayList<Character> charArray = new ArrayList<>();
-            String tempFullName = "";
+        String[] parts = fullName.split(" ");
 
-        for (int i = 0; i < fullName.indexOf(" "); i++) {
-            charArray.set(i, fullName.charAt(i));
-            prefix += charArray.get(i);
+        switch (parts.length) {
+            case 5:
+                return parts[3] + " " + parts[0] + " " + parts [1] + " " + parts[2] + ", " + parts[4];
+            case 4:
+                return parts[2] + " " + parts[0] + " " + parts[1] + ", " + parts[3];
+            case 2:
+                return parts[1] + ", " + parts[0];
         }
-
-        tempFullName = fullName.replaceAll(prefix, "");
-
-        for (int i = 0; i < tempFullName.indexOf(" "); i++) {
-            charArray.set(i, tempFullName.charAt(i));
-            firstName += charArray.get(i);
-        }
-
-        tempFullName = tempFullName.replaceAll(firstName, "");
-
-        for (int i = 0; i < tempFullName.indexOf(" "); i++) {
-            charArray.set(i, tempFullName.charAt(i));
-            middleName += charArray.get(i);
-        }
-
-        tempFullName = tempFullName.replaceAll(middleName, "");
-
-        for (int i = 0; i < tempFullName.indexOf(","); i++) {
-            charArray.set(i, tempFullName.charAt(i));
-            lastName += charArray.get(i);
-        }
-
-        tempFullName = tempFullName.replaceAll(lastName + ",", "");
-        suffix = tempFullName;
-
-        return lastName + " " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+        return "invalid name format";
     }
 }
