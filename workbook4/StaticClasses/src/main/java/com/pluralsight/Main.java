f7231c228324d7417d313ae6a137f55c256a1b1a
Nik <nikkusaka@gmail.com>
2025-10-21 14:24:13 -0700

    -First attempt didn't work trying different approach now

---
+++ workbook4/StaticClasses/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+import static com.pluralsight.NameFormatter.format;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(format("Dr. Mel B Johnson, PhD"));
+    }
+}
