9d34ad8c4998ce75a57dbfe88d5ddc142ac7c247
Nik <nikkusaka@gmail.com>
2025-10-23 09:45:36 -0700

    -Created a saiyan class to practice with static variables

---
+++ demos-teacher/JavaStaticPractice/src/main/java/com/pluralsight/Saiyan.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class Saiyan {
+
+    // instance variables unique for any saiyan
+    private String name;
+    private int powerLevel;
+
+    // static variables - shared by ALl saiyans
+    private static int totalSaiyans = 0;
+
+    public Saiyan(String name, int powerLevel) {
+        this.name = name;
+        this.powerLevel = powerLevel;
+        totalSaiyans++; // One extra saiyan has been created +1
+
+
+    }
+
+    public void setPowerLevel(int powerLevel) {
+        if (powerLevel > 0) {
+            this.powerLevel = powerLevel;
+        }
+    }
+
+    public int getTotalSaiyans() {
+        return totalSaiyans;
+    }
+}
