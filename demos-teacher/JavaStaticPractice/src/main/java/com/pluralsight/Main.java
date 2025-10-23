6042fdfdf4a0ffccd86463fde251d9de65f2b8c0
Nik <nikkusaka@gmail.com>
2025-10-23 09:45:36 -0700

    -Created a saiyan class to practice with static variables

---
+++ demos-teacher/JavaStaticPractice/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import static com.pluralsight.Saiyan.totalSaiyans;
+
+public class Main {
+    public static void main(String[] args) {
+        Saiyan s1 = new Saiyan("Goku", 10000);
+
+
+    }
+}
