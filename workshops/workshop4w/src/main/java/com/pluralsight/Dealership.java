5ab52472da485c71de347756dd3f0d99b754269d
Nik <nikkusaka@gmail.com>
2025-10-24 14:18:46 -0700

    -Added in all functionality for Dealership need to actually input the methods now

--- workshops/workshop4w/src/main/java/com/pluralsight/Dealership.java
+++ workshops/workshop4w/src/main/java/com/pluralsight/Dealership.java
@@ -7,7 +7,52 @@ public class Dealership {
     private String name;
     private String address;
     private String phone;
-
     private ArrayList<Vehicle> inventory;
+
+    public Dealership() {
+
+    }
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    Vehicle getVehiclesByPrice(int min, int max) {
+
+    }
+
+    Vehicle getVehiclesByMakeModel(String make, String model) {
+
+    }
+
+    Vehicle getVehiclesByYear(int min, int max) {
+
+    }
+
+    Vehicle getVehiclesByColor(String color) {
+
+    }
+
+    Vehicle getVehiclesByMileage(int min, int max) {
+
+    }
+
+    Vehicle getVehiclesByType(String vehicleType) {
+
+    }
+
+    Vehicle getAllVehicles() {
+
+    }
+
+    void addVehicle(Vehicle vehicle) {
+
+    }
+
+    void removeVehicle(Vehicle vehicle) {
+
+    }
 
 }
