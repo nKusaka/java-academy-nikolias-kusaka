679aa7a165b29e4e2b797a220c64d84a56eb654e
Nik <nikkusaka@gmail.com>
2025-10-27 10:18:51 -0700

    -Added getDealership method to dealershipfilemanager class

--- workshops/workshop4w/src/main/java/com/pluralsight/DealershipFileManager.java
+++ workshops/workshop4w/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,6 +1,51 @@
 package com.pluralsight;
+import java.io.*;
+import java.util.ArrayList;
 
 public class DealershipFileManager {
 
 
+    public Dealership getDealership() {
+        Dealership dealership = new Dealership();
+        String csvFile = "inventory.csv";
+        String input;
+        boolean header = true;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] dealershipData = input.split("\\|");
+
+                if (header) {
+                    String name = dealershipData[0];
+                    String address = dealershipData[1];
+                    String phone = dealershipData[2];
+                    header = false;
+                    dealership.setName(name);
+                    dealership.setAddress(address);
+                    dealership.setPhone(phone);
+
+                } else {
+                    Vehicle vehicle = getVehicle(dealershipData);
+                    dealership.addVehicle(vehicle);
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Could not read from file make sure formatting is correct");
+        }
+        return dealership;
+    }
+
+    private Vehicle getVehicle(String[] dealershipData) {
+        int vin = Integer.parseInt(dealershipData[0]);
+        int year = Integer.parseInt(dealershipData[1]);
+        String make = dealershipData[2];
+        String model = dealershipData[3];
+        String vehicleType = dealershipData[4];
+        String color = dealershipData[5];
+        int odometer = Integer.parseInt(dealershipData[6]);
+        double price = Double.parseDouble(dealershipData[7]);
+
+        return new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+    }
 }
