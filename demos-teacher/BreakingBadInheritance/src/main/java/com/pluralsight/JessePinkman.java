a53937b99d55be7c41ce79b2777e5c156faeec58
Nik <nikkusaka@gmail.com>
2025-10-28 09:30:12 -0700

    -Completed demo project

---
+++ demos-teacher/BreakingBadInheritance/src/main/java/com/pluralsight/JessePinkman.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class JessePinkman extends Chemist {
+
+    @Override
+    public void cook() {
+        System.out.println("Yeah science!");
+    }
+}
---
+++ demos-teacher/BreakingBadInheritance/src/main/java/com/pluralsight/WalterWhite.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class WalterWhite extends Chemist {
+
+    @Override
+    public void cook() {
+        System.out.println("I am the one who knocks!");
+    }
+}
