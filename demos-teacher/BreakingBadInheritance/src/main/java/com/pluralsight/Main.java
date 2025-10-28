88c3219d1b62bd61e8366ba2471b91d3707b8177
Nik <nikkusaka@gmail.com>
2025-10-28 09:30:12 -0700

    -Completed demo project

--- demos-teacher/BreakingBadInheritance/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/BreakingBadInheritance/src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,17 @@
 package com.pluralsight;
-
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        Chemist c1 = new WalterWhite();
+        Chemist c2 = new JessePinkman();
 
+        ArrayList<Chemist> chemists = new ArrayList<>();
+        chemists.add(c1);
+        chemists.add(c2);
+
+        for (Chemist chemist: chemists) {
+            chemist.cook();
+        }
     }
 }
