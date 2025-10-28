305c3947ce74b7a19988ee9b4dde8973e49fe8ef
Nik <nikkusaka@gmail.com>
2025-10-28 14:27:23 -0700

    -Demo project about inheritance and abstract classes

---
+++ demos-teacher/PokemonDemo/src/main/java/com/pluralsight/Togemon.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Togemon extends Pokemon {
+
+    public Togemon(String name) {
+        super(name);
+    }
+
+    @Override
+    public String Attack() {
+        return "Punch";
+    }
+}
