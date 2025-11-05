0527d80a0ad57f5c582288440ec34a57a6e0a1a6
Nik <nikkusaka@gmail.com>
2025-11-05 15:02:09 -0800

    -User is able to print out items using a for loop that does not worry about the type of item in the list

--- workbook6/CustomCollections/src/main/java/com/pluralsight/Main.java
+++ workbook6/CustomCollections/src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,13 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        FixedList<Object> list = new FixedList<>(2);
 
+        list.add(3);
+        list.add("Nik");
+
+        for(Object item: list.getItems()) {
+            System.out.println(item);
+        }
     }
 }
