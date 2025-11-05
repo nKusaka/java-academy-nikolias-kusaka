7f2be3bc95b50a8776baf492cfa1a6d4d91afd16
Nik <nikkusaka@gmail.com>
2025-11-05 14:55:52 -0800

    -Started new project to practice using generics

--- workbook6/WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
+++ workbook6/WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,25 @@
 package com.pluralsight;
-
+import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        List<Person> myFamily = new ArrayList<Person>();
+        myFamily.add(new Person("Dana", "Wyatt", 63));
+        myFamily.add(new Person("Zachary", "Westly", 31));
+        myFamily.add(new Person("Elisha", "Aslan", 14));
+        myFamily.add(new Person("Ian", "Auston", 16));
+        myFamily.add(new Person("Zephaniah", "Hughes", 9));
+        myFamily.add(new Person("Ezra", "Aiden", 17));
+
+        for(Person person: myFamily) {
+            System.out.println(person.getFirstName());
+        }
+
+        Collections.sort(myFamily);
+
+        for(Person person: myFamily) {
+            System.out.println(person.getFirstName());
+        }
     }
 }
