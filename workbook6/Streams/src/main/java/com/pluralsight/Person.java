198898ae581b2179a1e6bc23efc892a4d74cfde5
Nik <nikkusaka@gmail.com>
2025-11-05 15:06:27 -0800

    -Created a person class

---
+++ workbook6/Streams/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+}
