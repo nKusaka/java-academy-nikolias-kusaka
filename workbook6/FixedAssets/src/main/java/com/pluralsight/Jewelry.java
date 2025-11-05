148fada3b9117e675e8641a42410d48c1e2d6b75
Nik <nikkusaka@gmail.com>
2025-11-05 13:54:24 -0800

    -Started new project to practice using interfaces

--- workbook6/FixedAssets/src/main/java/com/pluralsight/Jewelry.java
+++ workbook6/FixedAssets/src/main/java/com/pluralsight/Jewelry.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
-public class Jewelry {
+public class Jewelry extends Asset {
+
+    // Instantiate class variables
+    private double karat;
+
+    // Parametrized Constructor
+    public Jewelry
 }
