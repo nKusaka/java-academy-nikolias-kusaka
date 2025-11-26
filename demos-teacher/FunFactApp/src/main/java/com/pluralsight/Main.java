2b88ca7aa7b4373b1b5db43c0122d5cf8423eafc
Nik <nikkusaka@gmail.com>
2025-11-26 09:31:48 -0800

    Updated my fun fact

--- demos-teacher/FunFactApp/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/FunFactApp/src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,6 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My fun fact:");
-        System.out.println("My favorite color is red");
+        System.out.println("I wanted to be a doctor when I was younger");
     }
 }
