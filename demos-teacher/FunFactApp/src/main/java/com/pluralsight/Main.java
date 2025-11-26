edf24c261e8e9fdaa2c66807a7feb1681b2a3191
Nik <nikkusaka@gmail.com>
2025-11-26 10:09:46 -0800

    Added one of my bucket list items

--- demos-teacher/FunFactApp/src/main/java/com/pluralsight/Main.java
+++ demos-teacher/FunFactApp/src/main/java/com/pluralsight/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My fun fact:");
         System.out.println("My dream job growing up was to become a doctor");
+        System.out.println("My one bucket list item:");
+        System.out.println("I want to try sky diving");
     }
 }
